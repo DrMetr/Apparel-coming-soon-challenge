e4e8740f3e4e7b42e530a544327e51636b49b44f
DrMetr <dashagamlo@mail.ru>
2025-03-16 16:35:22 +0300

    bit more done

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const email = document.querySelector("#email");
 const emailRegExp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 const submitButton = document.querySelector('#submit');
-const errorMessage = document.querySelector("#error");
+const error = document.querySelector("#error");
 
 submitButton.addEventListener('click', onSubmit);
 
 function validateEmail(emailInput){
     return emailRegExp.test(String(emailInput).toLowerCase());
-  }
+}
 
 function onSubmit(){
     if (validateEmail(email.value)) {
         email.classList.remove('errinput');
-        email.classList.remove('errinput');
-        email.style.border = '1px solid var(--grayishBlue)';
+        email.style.border = '1px solid black';
         email.classList.add('.errinput');
         error.style.visibility = 'hidden';
     } else {
         error.style.visibility = 'visible';
-        email.style.border = '2px solid var(--red)';
-        email.classList.add('.errinput');
+        email.style.border = '2px solid var(--SoftRed)';
+        email.classList.add('errinput');
     }
 }
